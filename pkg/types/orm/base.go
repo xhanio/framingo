fc6cb42d8fc78af52740880ebc12317587a73e32
xihan88 <hanx.azure22@gmail.com>
2025-12-04 19:47:01 -0800

    Remove: orm.BaseModel

--- pkg/types/orm/base.go
+++ pkg/types/orm/base.go
@@ -2,48 +2,26 @@ package orm
 
 import (
 	"fmt"
-	"time"
 )
 
 const NoVersion int64 = 0
 
-type Record interface {
-	GetID() int32
+type Record[T comparable] interface {
+	GetID() T
 	GetErased() bool
 	GetVersion() int64
 	TableName() string
 }
 
-type Referenced interface {
-	References() []Reference
+type Referenced[T comparable] interface {
+	References() []Reference[T]
 }
 
-type BaseModel struct {
-	ID        int32     `gorm:"primaryKey;autoIncrement;column:id;type:integer;not null"`
-	Erased    bool      `gorm:"column:erased;type:boolean;not null;default false"`
-	Hidden    bool      `gorm:"column:hidden;type:boolean"`
-	CreatedAt time.Time `gorm:"column:created_at;type:timestamp with time zone;not null;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"column:updated_at;type:timestamp with time zone;not null;autoUpdateTime"`
-	Version   int64     `gorm:"column:version;type:bigint"`
-}
-
-func (b BaseModel) GetID() int32 {
-	return b.ID
-}
-
-func (b BaseModel) GetErased() bool {
-	return b.Erased
-}
-
-func (b BaseModel) GetVersion() int64 {
-	return b.Version
-}
-
-type Reference struct {
+type Reference[T comparable] struct {
 	TableName string
-	ID        int32
+	ID        T
 }
 
-func (r Reference) Key() string {
-	return fmt.Sprintf("%s/%d", r.TableName, r.ID)
+func (r Reference[T]) Key() string {
+	return fmt.Sprintf("%s/%v", r.TableName, r.ID)
 }
